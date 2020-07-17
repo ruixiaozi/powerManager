e5a5d709d0c5b46d972650e6d34c9095a41fffc9
ruixiaozi <admin@ruixiaozi.com>
2020-07-17 09:59:13 +0800

    fix(运行模式): 修复运行模式 1.0.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,48 +13,68 @@ import java.util.Scanner;
  */
 public class Main {
 
+    private static final long SLEEP_TIME = 1000*60*2;
+
     public static void main(String[] args) throws PortInUseException {
 
-        ArrayList<String> list = SerialPortManager.findPorts();
-        System.out.println("start");
-        System.out.println("com list is:");
-        System.out.println(list);
-        for(int i = 0;i<list.size();i++){
+        while (true) {
+            ArrayList<String> list = SerialPortManager.findPorts();
+            System.out.println("start");
+            System.out.println("com list is:");
+            System.out.println(list);
+            String port = null;
+            for(int i = 0;i<list.size();i++){
 
-            if(list.get(i).indexOf("ttyUSB")!=-1){
-                System.out.println("choose :"+list.get(i));
-                SerialPort serialPort = SerialPortManager.openPort(list.get(i),115200);
-                SerialPortManager.addListener(serialPort, new SerialPortManager.DataAvailableListener() {
-                    @Override
-                    public void dataAvailable() {
-                        byte[] data = null;
-                        try {
-                            if (serialPort == null) {
-                                System.out.println("串口对象为空，监听失败！");
-                            } else {
-                                // 读取串口数据
-                                int re = SerialPortManager.readFromPort(serialPort);
-                                //System.out.print(re);
-                                if(re == '1'){
+                if(list.get(i).indexOf("ttyUSB")!=-1){
+                    port = list.get(i);
+                    break;
+                }
 
-                                    //关机
-                                    System.out.println(CommandUtil.run("shutdown -h now"));
-                                    System.exit(0);
-                                }
-
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            // 发生读取错误时显示错误信息后退出系统
-                            System.exit(0);
-                        }
-                    }
-                });
-                break;
             }
+            System.out.println("choose :"+port);
+            SerialPort serialPort = SerialPortManager.openPort(port,115200);
+            SerialPortManager.addListener(serialPort, new SerialPortManager.DataAvailableListener() {
+                @Override
+                public void dataAvailable() {
+                    byte[] data = null;
+                    try {
+                        if (serialPort == null) {
+                            System.out.println("串口对象为空，监听失败！");
+                        } else {
+                            // 读取串口数据
+                            int re = SerialPortManager.readFromPort(serialPort);
+                            //System.out.print(re);
+                            if(re == '1'){
 
+                                //关机
+                                System.out.println(CommandUtil.run("shutdown -h now"));
+                                System.exit(0);
+                            }
+
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        // 发生读取错误时显示错误信息后退出系统
+                        System.exit(0);
+                    }
+                }
+            });
+
+
+            //间隔2分钟
+            System.out.println("Infom:sleep 2 minutes...");
+            try {
+                Thread.sleep(SLEEP_TIME);
+                SerialPortManager.closePort(serialPort);
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("Error:sleep error");
+                System.out.println("ErrCause:" + e.getCause());
+                System.out.println("ErrMsg:" + e.getMessage());
+            }
         }
-
 
 
     }
